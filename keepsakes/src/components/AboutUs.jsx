bb5f5376dcf2df26d8b82dfc7b38f389d1d13a50
reemhd <reemhde@gmail.com>
2023-03-09 15:38:58 +0000

    about us]

--- keepsakes/src/components/AboutUs.jsx
+++ keepsakes/src/components/AboutUs.jsx
@@ -1,5 +1,28 @@
+import '../css/about-us.css'
+
 function AboutUs() {
-  return <p>About Us</p>;
+  return (
+    <div className="about-us-container">
+      <h1>Welcome to Keepsakes!</h1>
+      <p>
+        We are an online store that specialises in vintage items from the past.
+        Our inventory includes items from three categories: Electronics, Relics,
+        and Household.
+      </p>
+      <p>
+        Whether you're a collector, a history buff, or just someone who
+        appreciates the beauty and craftsmanship of vintage items, you'll find
+        something you love at Keepsakes. Our inventory is carefully curated to
+        ensure that we offer only the best and most interesting items, and we're
+        always on the lookout for new treasures to add to our collection.
+      </p>
+      <p>
+        Thank you for choosing Keepsakes as your source for vintage items. We
+        hope you enjoy browsing our inventory and finding the perfect item for
+        your collection or home.
+      </p>
+    </div>
+  );
 }
 
 export default AboutUs;
